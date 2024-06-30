50ab6ab9d378a5730bdf426541f813c58335a3df
blu <172100206+blu-berricake@users.noreply.github.com>
2024-06-30 22:39:30 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function tenPull() {
+    count += 10
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    count = 0
+    countEl.textContent = 0
+    saveEl.textContent = "5 star count:"
+}
